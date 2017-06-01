838e5f3d08fdc190c1a596073d0477b9f69ebd55
Doug Keester <dkeester@gmail.com>
2017-06-01 16:36:11 -0600

    Changes to be committed: 	modified:   calc/Cargo.toml 	modified:   calc/src/main.rs 	modified:   guessing_game/Cargo.toml 	modified:   guessing_game/src/main.rs
    Committing updates... I think.

--- calc/src/main.rs
+++ calc/src/main.rs
@@ -1,3 +1,35 @@
+/*
+ * A commandline based four function calculator.
+ */
+use std::io::{self, Write};
+
 fn main() {
-    println!("Hello, world!");
-}
+    println!("A simple four function calculator");
+    println!("Type \"quit\" or \"exit\" to quit.");
+    println!();
+
+    // process input in a loop indefinitely.
+    loop {
+        emit_prompt();
+
+        let mut exp = String::new();
+
+        io::stdin().read_line(&mut exp)
+            .expect("Failed to read line");
+        let exp = exp.trim();
+
+        // allow a graceful exit from the program.
+        if exp == "quit" { // '==' can do string comparison
+            break;
+        } else if exp == "exit" {
+            break;
+        }
+
+        println!("expression: {}", exp);
+    } // end loop
+} // end main
+
+fn emit_prompt() {
+    print!("> ");
+    io::stdout().flush().unwrap();
+} // end emit_prompt
