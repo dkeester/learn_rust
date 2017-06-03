41e4ad1ccc9f1afe2f5ddd29f02201d214bd5cd2
Doug Keester <dkeester@gmail.com>
2017-06-03 00:04:26 -0600

    everything seems to work. finished?

--- calc/src/main.rs
+++ calc/src/main.rs
@@ -5,7 +5,9 @@ use std::io::{self, Write};
 
 fn main() {
     println!("A simple four function calculator");
+    println!();
     println!("Type \"quit\" or \"exit\" to quit.");
+    println!("Operators are: +, -, *, and /");
     println!();
 
     // process input in a loop indefinitely.
@@ -26,6 +28,42 @@ fn main() {
         }
 
         println!("expression: {}", exp);
+
+        if exp.contains('+') {
+            //println!("Addition!");
+            let v: Vec<&str> = exp.split('+').collect();
+
+            let (x, y) = (v[0].parse::<i64>().unwrap(),
+                        v[1].parse::<i64>().unwrap());
+
+            println!("Answer: {}", add(x, y));
+        } else if exp.contains('-') {
+            //println!("Subtraction");
+            let v: Vec<&str> = exp.split('-').collect();
+
+            let (x, y) = (v[0].parse::<i64>().unwrap(),
+                        v[1].parse::<i64>().unwrap());
+
+            println!("Answer: {}", sub(x, y));
+        } else if exp.contains('*') {
+            //println!("Multiplication");
+            let v: Vec<&str> = exp.split('*').collect();
+
+            let (x, y) = (v[0].parse::<i64>().unwrap(),
+                        v[1].parse::<i64>().unwrap());
+
+            println!("Answer: {}", mul(x, y));
+        } else if exp.contains('/') {
+            //println!("Division");
+            let v: Vec<&str> = exp.split('/').collect();
+
+            let (x, y) = (v[0].parse::<i64>().unwrap(),
+                        v[1].parse::<i64>().unwrap());
+
+            println!("Answer: {}", div(x, y));
+        } else {
+            println!("Error! Try again.");
+        }
     } // end loop
 } // end main
 
@@ -33,3 +71,24 @@ fn emit_prompt() {
     print!("> ");
     io::stdout().flush().unwrap();
 } // end emit_prompt
+
+fn add(x: i64, y: i64) -> i64 {
+    x+y
+}
+
+fn sub(x: i64, y: i64) -> i64 {
+    x-y
+}
+
+fn mul(x: i64, y: i64) -> i64 {
+    x*y
+}
+
+fn div(x: i64, y: i64) -> i64 {
+    if y == 0 {
+        println!("Division by zero! Answer undefined!");
+        0
+    } else {
+        x/y
+    }
+}
