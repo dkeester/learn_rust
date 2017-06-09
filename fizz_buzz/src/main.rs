63277ddb9dbbbd5a7a19b48e105c6cf7af4c746a
Doug Keester <dkeester@gmail.com>
2017-06-09 01:12:10 -0600

    working fizz buzz

--- fizz_buzz/src/main.rs
+++ fizz_buzz/src/main.rs
@@ -1,20 +1,17 @@
 // A quick implementation of the fizz/buzz game
-use std::string::String;
 
 fn main() {
-    let mut output = String::new();
-
     for x in 1..100 {
+        let mut output = format!("{} ", x);
+
         if x % 3 == 0 {
-            output.push_str("fizz");
-        } else if x % 5 == 0 {
-            output.push_str("buzz");
-        } else {
-            output = format!("{}", x);
+            output = output + "fizz ";
+        }
+
+        if x % 5 == 0 {
+            output = output + "buzz ";
         }
 
         println!("{}", output);
-
-        output = "".to_string();
     }
 }
