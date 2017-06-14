7d34db91232ab5c21ccec30b8030da85305958b9
Doug Keester <dkeester@gmail.com>
2017-06-14 16:08:41 -0600

    small changes

--- file_read_print/src/main.rs
+++ file_read_print/src/main.rs
@@ -1,3 +1,13 @@
+// Open a file, or read a file on stdin,
+// and print it to stdout with line numbers
+use std::io;
+use std::io::prelude::*;
+use std::fs::File;
+
 fn main() {
-    println!("Hello, world!");
+    let lines = "hello\nworld".lines();
+
+    for (linenumber, line) in lines.enumerate() {
+        println!("{}: {}", linenumber, line);
+    }
 }
