5b8f537e17a404442de551aa5653a650dc2e03c9
dkeester <dkeester@gmail.com>
2017-06-26 16:43:12 -0600

    minor changes, still incomplete

--- temp_convert/src/main.rs
+++ temp_convert/src/main.rs
@@ -1,3 +1,42 @@
+use std::io::{self, Write};
+
+fn emit_prompt() {
+    print!("temp> ");
+    io::stdout().flush().unwrap();
+} // end emit_prompt
+
+/*fn to_celsius(temp: f64) -> f64 {
+    0.0
+}
+
+fn to_fahrenheit(temp: f64) -> f64 {
+    0.0
+}
+
+fn to_kelvin(temp: f64) -> f64 {
+    0.0
+}
+*/
 fn main() {
-    println!("Hello, world!");
+    println!("Temperature Converter");
+    println!("please enter a temperature followed by a letter for the units.");
+    println!("Examples: 23f, 12.6c, 42.0k (Fahrenheit, Celsius, Kelvin)");
+
+    loop {
+        emit_prompt();
+
+        let mut value = String::new();
+        io::stdin().read_line(&mut value)
+            .expect("Failed to read line");
+        let value = value.trim();
+
+        // allow a graceful exit from the program.
+        if value == "quit" {
+            break;
+        } else if value == "exit" {
+            break;
+        }
+
+        println!("You gave me: {}", value);
+    }
 }
