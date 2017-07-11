6a1131668e72ec582daf5f65f5ad4016c3f15e79
Doug Keester <dkeester@gmail.com>
2017-07-10 23:53:18 -0600

    initial changes to use a Struct.

--- temp_convert/src/main.rs
+++ temp_convert/src/main.rs
@@ -5,18 +5,18 @@ fn emit_prompt() {
     io::stdout().flush().unwrap();
 } // end emit_prompt
 
-/*fn to_celsius(temp: f64) -> f64 {
-    0.0
+struct Temp {
+    deg_celsius: f64,
+    deg_fahrenheit: f64,
+    deg_kelvin: f64,
 }
 
-fn to_fahrenheit(temp: f64) -> f64 {
-    0.0
+impl Temp {
+    fn convert(temp: f64, from_units: char, to_units: char) -> f64 {
+        0.0
+    }
 }
 
-fn to_kelvin(temp: f64) -> f64 {
-    0.0
-}
-*/
 fn main() {
     println!("Temperature Converter");
     println!("please enter a temperature followed by a letter for the units.");
@@ -26,8 +26,7 @@ fn main() {
         emit_prompt();
 
         let mut value = String::new();
-        io::stdin().read_line(&mut value)
-            .expect("Failed to read line");
+        io::stdin().read_line(&mut value).expect("Failed to read line");
         let value = value.trim();
 
         // allow a graceful exit from the program.
